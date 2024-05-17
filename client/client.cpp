d28d96577218524c986a71431c4887b764fd261d
BenLevintan <126006202+BenLevintan@users.noreply.github.com>
2024-05-17 10:50:29 +0300

    Update client.cpp
    added terminal arguments for the main function and terminal output for those arguments (for testing)

--- client/client.cpp
+++ client/client.cpp
@@ -8,13 +8,23 @@
 #include <unistd.h>
 #include "std_lib_facilities.h"
 
+using namespace std;
 
 // following zoom recording of Michael from 19-03
-int main(int argc, char* argv[]){
+int main(int argc, char *argv[]) {
+    // template for quick and easy testing //
+    // string input_file = argv[1];
+    // string output_file = argv[2];
+    // string old_string = argv[3];
+    // string new_string = argv[4];
 
-    string ip = argv[1];
-    int source = atoi(argv[2]);
-    int port = atoi(argv[3]);
+    std::string ip = argv[1];
+    int port = atoi(argv[2]);
+    int source = atoi(argv[3]);
+    int destination = atoi(argv[4]);
+
+    cout << "ip adrress: " << ip << "\nport num: " << port << "\nsource: " << source << "\ndestination: " << destination << endl;
+
 
     int fd = socket(AF_INET, SOCK_STREAM, 0);
     sockaddr_in addr = {0};
