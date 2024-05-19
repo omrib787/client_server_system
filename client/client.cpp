a9f16169d5c7b6506d23ca3fe10d559d0e480189
BenLevintan <126006202+BenLevintan@users.noreply.github.com>
2024-05-19 13:52:37 +0300

    Update client.cpp
    - added the message that should be delivered to server.cpp

--- client/client.cpp
+++ client/client.cpp
@@ -27,22 +27,28 @@ int main(int argc, char *argv[]) {
     addr.sin_family = AF_INET;
     addr.sin_addr.s_addr = inet_addr(ip.c_str());
     addr.sin_port = htons(port);
-    bind(fd, (sockaddr*)&addr, sizeof(addr));
-    listen(fd, 5);
+    connect(fd, (sockaddr*)&addr, sizeof(addr));
 
-    for(;;){
-        int fd2 = accept(fd, NULL, NULL);
-        if (fork() == 0){
-            for ( int i = 0 ; i < 4 ; ++i){
-                string s = to_string(i) + "\n";
-                write(fd2, &s[0], s.size());
-                // write(fd2, &source, sizeof(source));
-                // write(fd2, &destination, sizeof(destination));
-                sleep(1);
-            }
-            close(fd2);
-            exit(0);
-        }
-    }
+
+    // Send source and destination to the server
+    string payload = to_string(source) + "," + to_string(destination);
+    send(fd, payload.c_str(), payload.length(), 0);
+
     close(fd);
+    return 0;
 }
+
+    // for(;;){
+    //     int fd2 = accept(fd, NULL, NULL);
+    //     if (fork() == 0){
+    //         for ( int i = 0 ; i < 4 ; ++i){
+    //             string s = to_string(i) + "\n";
+    //             write(fd2, &s[0], s.size());
+    //             // write(fd2, &source, sizeof(source));
+    //             // write(fd2, &destination, sizeof(destination));
+    //             sleep(1);
+    //         }
+    //         close(fd2);
+    //         exit(0);
+    //     }
+    // }
